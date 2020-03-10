52e35f6af55b8e5f479a9bb581fe44e916762c01
otran9 <36254277+otran9@users.noreply.github.com>
2020-03-09 22:23:36 -0400

    have something close to clock, just need timer

--- app/src/main/java/com/example/shakeoff/MainActivity.java
+++ app/src/main/java/com/example/shakeoff/MainActivity.java
@@ -6,11 +6,14 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 import android.content.Intent;
 import android.os.Bundle;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -21,33 +24,41 @@ Alarm: Implement an alarm to do something at a time. (CRUD) Set multiple alarms 
  */
 
 public class MainActivity extends AppCompatActivity {
-    int hours, minutes, seconds, time;
+    int hour, setHour, minute, time;
     EditText hrText, minText, secText;
     Button button;
     Button testAlarm;
     Switch AM_PM;
+    String AM_PM_String = " AM";
     Timer alarm;
-    boolean timerSet;
+    boolean timerSet, isPM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        hours = 0;
-        minutes = 0;
-        seconds = 0;
+        hour = 0;
+        setHour = 0;
+        minute = 0;
         time = 0;
+
         hrText = findViewById(R.id.hour);
         minText = findViewById(R.id.minute);
         button = findViewById(R.id.button);
         testAlarm = findViewById(R.id.test);
+
         AM_PM = findViewById(R.id.AM_PM);
+        AM_PM.setText("AM");
         timerSet = false;
+        isPM = false;
+
+        /*
         Calendar cal = Calendar.getInstance();
         cal.setTimeInMillis(System.currentTimeMillis());
-
-        cal.clear();
+        Toast.makeText(getApplicationContext(), ("Hour: " + cal.get(Calendar.HOUR_OF_DAY) + " Minute: " + cal.get(Calendar.MINUTE)), Toast.LENGTH_LONG).show();
+        //cal.clear();
+         */
 
         hrText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -58,11 +69,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 if (charSequence.length() == 0) {
-                    hours = 0;
+                    hour = 0;
                     return;
                 }
-                hours = Integer.parseInt(charSequence.toString());
-                updateTime();
+                hour = Integer.parseInt(charSequence.toString());
+                if (hour > 12) {
+                    hour = 12;
+                }
+                //updateTime();
             }
 
             @Override
@@ -70,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
         minText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -79,14 +94,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 if (charSequence.length() == 0) {
-                    minutes = 0;
+                    minute = 0;
                     return;
                 }
-                minutes = Integer.parseInt(charSequence.toString());
-                if (minutes > 59) {
-                    minutes = 0;
+                minute = Integer.parseInt(charSequence.toString());
+                if (minute > 59) {
+                    minute = 59;
                 }
-                updateTime();
+                //updateTime();
             }
 
             @Override
@@ -95,11 +110,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        AM_PM.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    isPM = true;
+                    Toast.makeText(getApplicationContext(), ("isPM = true"), Toast.LENGTH_LONG).show();
+                    AM_PM.setText("PM");
+                    AM_PM_String = " PM";
+                } else {
+                    isPM = false;
+                    Toast.makeText(getApplicationContext(), ("isPM = false"), Toast.LENGTH_LONG).show();
+                    AM_PM.setText("AM");
+                    AM_PM_String = " AM";
+                }
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             Intent alarmIntent = new Intent(getApplicationContext(), AlarmActivity.class);
-
             @Override
             public void onClick(View v) {
+                if (isPM) {
+                    setHour = hour + 12;
+                } else {
+                    setHour = hour;
+                }
+                Calendar cal = Calendar.getInstance();
+                cal.setTimeInMillis(System.currentTimeMillis());
+                int currentHour = cal.get(Calendar.HOUR_OF_DAY);
+                int currentMinute = cal.get(Calendar.MINUTE);
+                Toast.makeText(getApplicationContext(), ("Current hour: " + currentHour + " Current minute: " + currentMinute + "\n"
+                                                    + "Hour: " + hour + " Set hour: " + setHour + " Minute: " + minute), Toast.LENGTH_LONG).show();
+                if (isTime(currentHour, currentMinute, hour, minute)) {
+                    startActivity(alarmIntent);
+                }
+                /*
                 alarm = new Timer();
                 alarm.schedule(new TimerTask() {
                     @Override
@@ -107,6 +153,7 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(alarmIntent);
                     }
                 }, time);
+                 */
             }
         });
 
@@ -126,8 +173,19 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    boolean isTime(int currentHour, int currentMinute, int hour, int minute) {
+        //Toast.makeText(getApplicationContext(), ("Alarm set for " + hour + ":" + minute + AM_PM_String), Toast.LENGTH_LONG).show();
+        boolean isTime = false;
+        if (setHour == currentHour) {
+            if (minute == currentMinute) {
+                isTime = true;
+            }
+        }
+        return isTime;
+    }
+
     void updateTime() {
-        time = (3600000 * hours) + (60000 * minutes);
+        time = (3600000 * hour) + (60000 * minute);
     }
     /*
      */
